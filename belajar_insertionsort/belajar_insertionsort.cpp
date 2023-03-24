871e4dafcd5a71f9089939eb515875dbf0b77cee
ImamDwiYanPermana <imampermana224@gmail.com>
2023-03-24 15:45:58 +0700

    menambhkan fungsi pengulangan yaitu dengan for dan menampilkan pesan dengan cout dan cin

--- belajar_insertionsort/belajar_insertionsort.cpp
+++ belajar_insertionsort/belajar_insertionsort.cpp
@@ -13,5 +13,11 @@ void input() {
 	cout << "=====================" << endl;
 	cout << "Masukan Element Array" << endl;
 	cout << "=====================" << endl;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ":";
+		cin >> arr[i];
+	}
 
 }
